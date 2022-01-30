5776f3a5e04e1a42725c7c2df52c0154bd90440f
DESKTOP-V52EIS0\Kosmosas <laurynasriauka98@gmail.com>
2022-01-30 12:50:42 +0200

    Save.

--- Generateprojects.bat
+++ Generateprojects.bat
@@ -1,2 +1,2 @@
-call Vendor\Bin\premake\premake5.exe vs 2017
+call Vendor\Bin\premake\premake5.exe vs2017
 PAUSE
--- premake5.lua
+++ premake5.lua
@@ -1,37 +1,132 @@
-workspace "ArticFox"
+workspace "ArcticFox"
 	architecture "x64"
+
+	startproject "ArcticFox"
 	
 	configurations {
 		"Debug",
-		"Release",
-		"Distribution"
+		"Release"
 	}
-	
+
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"GLFW/include/GLFW/glfw3.h",
+		"GLFW/include/GLFW/glfw3native.h",
+		"GLFW/src/glfw_config.h",
+		"GLFW/src/context.c",
+		"GLFW/src/init.c",
+		"GLFW/src/input.c",
+		"GLFW/src/monitor.c",
+		"GLFW/src/vulkan.c",
+		"GLFW/src/window.c"
+	}
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"GLFW/src/x11_init.c",
+			"GLFW/src/x11_monitor.c",
+			"GLFW/src/x11_window.c",
+			"GLFW/src/xkb_unicode.c",
+			"GLFW/src/posix_time.c",
+			"GLFW/src/posix_thread.c",
+			"GLFW/src/glx_context.c",
+			"GLFW/src/egl_context.c",
+			"GLFW/src/osmesa_context.c",
+			"GLFW/src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"GLFW/src/win32_init.c",
+			"GLFW/src/win32_joystick.c",
+			"GLFW/src/win32_monitor.c",
+			"GLFW/src/win32_time.c",
+			"GLFW/src/win32_thread.c",
+			"GLFW/src/win32_window.c",
+			"GLFW/src/wgl_context.c",
+			"GLFW/src/egl_context.c",
+			"GLFW/src/osmesa_context.c"
+		}
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
 
 project "AppFrame"
 	location "AppFrame"
 	kind "SharedLib"
 	language "C++"
-	targedir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	file {
-		"%{prj.name}/src/**.h"
-		"%{prj.name}/src/**.hpp"
-		"%{prj.name}/src/**.cpp"
+	files {
+		"%{prj.name}/%{prj.name}/src/**.h",
+		"%{prj.name}/%{prj.name}/src/**.hpp",
+		"%{prj.name}/%{prj.name}/src/**.cpp"
 	}
 
 	includedirs {
-		"%{prj.name}/Libraries/AF/include/"
+		"%{prj.name}/Libraries/AL/include/",
+		"%{prj.name}/Libraries/Entt/include/GL/",
+		"%{prj.name}/Libraries/glew/include/GL/",
+		"%{prj.name}/Libraries/glfw/include/",
+		"%{prj.name}/Libraries/glm/glm/",
+		"%{prj.name}/Libraries/spdlog/include/",
+		"%{prj.name}/Libraries/stb/include/",
+		"%{prj.name}/%{prj.name}/src/"
+	}
+	libdirs {
+		"%{prj.name}/Vendor/AL/lib/Win64/",
+		"%{prj.name}/Vendor/glew/lib/Win64/"
+	}
+
+	links {
+		"GLFW",
+		"OpenAL32",
+		"glew32",
+		"opengl32.lib"
 	}
 
 	filter "system:windows"
-		ccpdialect "c++17"
+		cppdialect "c++17"
 		staticruntime "On"
 		systemversion "latest"
 
-		define {
+		defines {
 			""
 		}
 		
@@ -40,33 +135,40 @@ project "AppFrame"
 		}
 
 		filter "configurations:Debug"
-			define "DEBUG"
+			defines "DEBUG"
 			symbols "On"
 
 		filter "configurations:Release"
-			define "RELEASE"
+			defines "RELEASE"
 			optimize "On"
 
 		filter "configurations:Distribution"
-			define "DISTRIBUTION"
+			defines "DISTRIBUTION"
 			optimize "On"
 
-project "ArticFox"
-	location "ArticFox"
+project "ArcticFox"
+	location "ArcticFox"
 	kind "ConsoleApp"
 	language "C++"
-	targedir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	file {
-		"%{prj.name}/src/**.h"
-		"%{prj.name}/src/**.hpp"
-		"%{prj.name}/src/**.cpp"
+	files {
+		"%{prj.name}/**.h",
+		"%{prj.name}/**.hpp",
+		"%{prj.name}/**.cpp"
 	}
 
 	includedirs {
-		"%{prj.name}/Libraries/AF/include/",
-		""
+		"AppFrame/Libraries/AL/include/",
+		"AppFrame/Libraries/Entt/include/GL/",
+		"AppFrame/Libraries/glew/include/GL/",
+		"AppFrame/Libraries/glfw/include/",
+		"AppFrame/Libraries/glm/",
+		"AppFrame/Libraries/spdlog/include/",
+		"AppFrame/Libraries/stb/include/",
+		"AppFrame/AppFrame/src/",
+		"ArcticFox/"
 	}
 
 	links {
@@ -74,22 +176,22 @@ project "ArticFox"
 	}
 
 	filter "system:windows"
-		ccpdialect "c++17"
+		cppdialect "c++17"
 		staticruntime "On"
 		systemversion "latest"
 
-		define {
+		defines {
 			""
 		}
 
 		filter "configurations:Debug"
-			define "DEBUG"
+			defines "DEBUG"
 			symbols "On"
 
 		filter "configurations:Release"
-			define "RELEASE"
+			defines "RELEASE"
 			optimize "On"
 
 		filter "configurations:Distribution"
-			define "DISTRIBUTION"
+			defines "DISTRIBUTION"
 			optimize "On"
